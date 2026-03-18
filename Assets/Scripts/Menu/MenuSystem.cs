1fd232833bb11dd11012ea08acc4fa31ea34e9cc
TomasPicadoPiedra <tomasspicado246@gmail.com>
2026-03-17 21:01:19 -0600

    Creacion del Menu
    Desarrollo del menu principal

---
+++ Assets/Scripts/Menu/MenuSystem.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSystem : MonoBehaviour
+{
+    public void Jugar()
+    {
+
+        SceneManager.LoadScene("escenaPrincipal");
+
+    }
+
+    public void Salir()
+    {
+
+        Application.Quit();
+
+    }
+
+}
+
